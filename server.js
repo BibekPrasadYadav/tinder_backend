0c644d1f191416cfaad72f6809cffea7b47e85c0
Bibek <bibek.roman1@gmail.com>
2024-11-03 14:09:51 +0530

    updated api

--- server.js
+++ server.js
@@ -8,16 +8,27 @@ const app = express();
 app.use(express.json());
 
 app.post("/user", async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const userData = new User(req.body);
 
   try {
-    const result = await userData.save();
-    console.log(result);
+    const allowedColumns = ["about", "lastName", "age", "photoUrl", "skills"];
+
+    const isAllowed = Object.keys(req.body).every((key) =>
+      allowedColumns?.includes(key)
+    );
+
+    console.log(isAllowed);
+    if (isAllowed) {
+      // res.status(500).send({ code: 500, message: "Not allowed" });
+      throw new Error("Not allowed");
+    }
+
+    // const result = await userData.save();
+    // console.log(result);
     res.status(201).send({ code: 201, message: "User Added Successfully!" });
   } catch (error) {
-    console.log(error);
-    res.status(500).send({ code: 500, message: error });
+    res.status(500).send({ code: 500, message: error.message });
   }
 });
 
@@ -33,8 +44,10 @@ app.get("/user", async (req, res) => {
 app.delete("/user", async (req, res) => {
   const { userId } = req.body;
   try {
-    const deletedUser = await User.findByIdAndDelete(userId);
-    console.log(deletedUser);
+    const reqBodyValue = Object.values(req.body)?.map((value) => value);
+    console.log(reqBodyValue);
+    // const deletedUser = await User.findByIdAndDelete(userId);
+    // console.log(deletedUser);
     res.status(200).send({ code: 200, message: "Deleted Successfully!" });
   } catch (error) {
     res.status(500).send({ code: 500, message: error });
@@ -43,6 +56,9 @@ app.delete("/user", async (req, res) => {
 
 app.patch("/user", async (req, res) => {
   const { userId } = req.body;
+
+  const data = Object.keys(req.body).map((key) => key);
+  console.log(data);
   try {
     const updatedUser = await User.findByIdAndUpdate(userId, req.body);
     console.log(updatedUser);
